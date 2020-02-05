e99024badc15eaf7d8227216dcc7b22a79fd18e7
Puneet Gupta <puneetg@microsoft.com>
2020-02-05 21:54:02 +0530

    Adding a Crashing page

---
+++ WebCampGit/ProcessCrash.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProcessCrash.aspx.cs" Inherits="demomvp.ProcessCrash" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ WebCampGit/ProcessCrash.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class ProcessCrash : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var ts = new ThreadStart(MyCrashingFunction);
+            var t = new Thread(ts);
+            t.Start();
+
+            Response.Write("Thread Started");
+        }
+
+        void MyCrashingFunction()
+        {
+            throw new ApplicationException("Catch me if you can!");
+        }
+    }
+}
