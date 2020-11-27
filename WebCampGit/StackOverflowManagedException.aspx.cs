8a53cf5c714129e571151802162304594f2f5393
Puneet Gupta <puneetg@microsoft.com>
2020-11-27 18:00:17 +0530

    Adding Stack Overflow page

---
+++ WebCampGit/StackOverflowManagedException.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class StackOverflowManagedException : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var ex = new Example();
+            ex.Execute();
+        }
+    }
+
+    public class Example
+    {
+        private const int MAX_RECURSIVE_CALLS = Int32.MaxValue;
+        static int ctr = 0;
+
+
+        public void Execute()
+        {
+            ctr++;
+
+            if (ctr <= MAX_RECURSIVE_CALLS)
+                Execute();
+
+            ctr--;
+        }
+    }
+}
