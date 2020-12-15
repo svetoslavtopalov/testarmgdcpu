06e9481096054b2c441d222d3ec0d182d5ea7ab3
Puneet Gupta <puneetg@microsoft.com>
2020-12-15 17:25:11 +0530

    Adding FailFast page

---
+++ WebCampGit/FailFast.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class FailExit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["exception"] !=null)
+            {
+                Environment.FailFast("calling Environment.FailFast", new ApplicationException("Some custom exception"));
+            }
+            else
+            {
+                Environment.FailFast("calling Environment.FailFast");
+            }
+        }
+    }
+}
