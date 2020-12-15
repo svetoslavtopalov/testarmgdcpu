e749347fd181085d3694bac99920779a67dca986
Puneet Gupta <puneetg@microsoft.com>
2020-12-15 17:41:33 +0530

    formatting crashvars page

--- WebCampGit/CrashVars.aspx.cs
+++ WebCampGit/CrashVars.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,14 +13,15 @@ namespace demomvp
     {
         protected void Page_Load(object sender, EventArgs eg)
         {
+            var builder = new StringBuilder();
             foreach (DictionaryEntry e in System.Environment.GetEnvironmentVariables())
             {
                 if(e.Key.ToString().ToUpper().Contains("CRASH"))
                 {
-                    Response.Write(e.Key + ":" + e.Value + "<br/>");
+                    builder.AppendLine(e.Key + ":" + e.Value + "<br/>");
                 }
-                
             }
+            lblMessage.Text = builder.ToString();
         }
     }
 }
