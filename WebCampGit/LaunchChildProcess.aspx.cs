8448644c8badf047a8b133f2784eaf1209578e22
Puneet Gupta <puneetg@microsoft.com>
2021-06-04 19:39:51 +0530

    Adding page to launch Child Processes

---
+++ WebCampGit/LaunchChildProcess.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class LaunchChildProcess : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Process.Start("ping.exe", "127.0.0.1 -n 30");
+            lblMessage.Text = $"Lanched child process at {DateTime.UtcNow} UTC";
+        }
+    }
+}
